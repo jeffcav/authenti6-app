98a6662019742b809f4a94bb2d7d5fee5b6b9c8f
Jefferson Cavalcante <recife.rodrigo@gmail.com>
2022-02-07 03:26:51 -0300

    Add style to ambient sound app

--- backend/services/ambient-sound/app.py
+++ backend/services/ambient-sound/app.py
@@ -1,11 +1,14 @@
 import json
 import spotipy
-from flask import Flask, url_for, request
+from flask import Flask, url_for, request, render_template, flash, g
 from spotipy.oauth2 import SpotifyOAuth
 
 app = Flask(__name__)
 
-available_songs = {
+# This is not actually secret, used for testing purposes only
+app.secret_key = "0aJrYfARfCKzv6gXjhs4SZ"
+
+songs_available = {
     'spotify:track:0aJrYfARfCKzv6gXjhs4SZ': "Jack Jonhson - Upside down",
     'spotify:track:0vT797IpZpF5o1XLCnWCUL': "Ferrugem - É Natural",
     'spotify:track:3tc8Z4lGzOIwmIVftsxO6o': "Marilia Mendonça - Graveto"
@@ -19,35 +22,55 @@ def get_credentials(services_file="credentials.json"):
         return json.load(read_file)
 
 def play_song(song_id):
+    error = None
     scope = "user-read-playback-state,user-modify-playback-state"
     sp = spotipy.Spotify(client_credentials_manager=SpotifyOAuth(scope=scope))
 
-    # Get available devices
     available_devices = sp.devices()
     try:
         device_id = available_devices['devices'][0]['id']
     except:
-        return "<html><body style=\"background-color:powderblue; font-family:verdana;\">No device available</body></html>\n"
+        error = "No device available"
+        
+    if error is None:
+        try:
+            sp.start_playback(device_id = device_id, uris=[song_id])
+            print("Playing: ", sp.currently_playing())
+        except spotipy.exceptions.SpotifyException:
+            error = "Could not play song"
+    
+    return error
 
-    # Play song
+def get_currently_playing_song():
+    scope = "user-read-playback-state,user-modify-playback-state"
+    sp = spotipy.Spotify(client_credentials_manager=SpotifyOAuth(scope=scope))
     try:
-        sp.start_playback(device_id = device_id, uris=[song_id])
-    except spotipy.exceptions.SpotifyException:
-        return "<html><body style=\"background-color:powderblue; font-family:verdana;\">Could not play song</body></html>\n"
-
-    return "<html><body style=\"background-color:powderblue; font-family:verdana;\">Now playing: {}<br/><a href={}>See songs list</a></body></html>\n".format(available_songs[song_id], url_for('index'))
+        name = sp.currently_playing()['item']['name']
+        return name
+    except:
+        return None
 
 @app.route('/index')
 @app.route('/')
 def index():
-    songs_list = ""
-    for song_id, song_name in available_songs.items():
-        songs_list += "<a href=\"{}?song={}\">{}<br/><br/>".format(url_for('play'), song_id, song_name)
-
-    html = HTML_HEAD + "{}".format(songs_list) + HTML_TAIL
-    return html
+    return render_template('songs.html', songs = songs_available)
 
 @app.route('/play', methods=['GET'])
 def play():
     song_id = request.args.get('song')
-    return play_song(song_id)
+    
+    error = play_song(song_id)
+    if error is None:
+        g.is_playing = True
+    else:
+        flash(error)
+        g.is_playing = False
+
+    current_song = get_currently_playing_song()
+    if current_song is None:
+        flash("Could not determine currently playing song")
+        g.current_song = None
+    else:
+        g.current_song = current_song
+
+    return render_template('play.html')
